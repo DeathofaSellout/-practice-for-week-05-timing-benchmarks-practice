0b16915fd96bda1d89677a8ea3fee79f782d331e
Erick Recher <deathofasellout@yahoo.com>
2022-05-29 17:49:33 -0700

    problems 1 and 2 solved

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,21 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let answer = []
+  for (let i = 1; i <= 10; i+=1) {
+    answer.push(addNums(i * increment));
+  }
+  return answer;
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  let answer = []
+  for (let i = 1; i <= 10; i += 1) {
+    answer.push(addManyNums(i * increment));
+  }
+  return answer;
 }
 
 module.exports = [addNums10, addManyNums10];
