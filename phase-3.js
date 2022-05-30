6d5e495379a488ad0fe1c3c4dc117e805653cda1
Erick Recher <deathofasellout@yahoo.com>
2022-05-29 18:10:22 -0700

    done

--- phase-3.js
+++ phase-3.js
@@ -4,7 +4,14 @@ function addNums10Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
 
-  // Your code here
+  let answer = []
+  for (let i = 1; i <= 10; i += 1) {
+    startTime = Date.now();
+    answer.push(addNums(i * increment));
+    endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+  }
+  return answer;
 
 }
 
@@ -12,18 +19,27 @@ function addNums10Timing(increment) {
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
 // Then, add timing code
-
-  // Your code here
-
+  let answer = []
+  for (let i = 1; i <= 10; i += 1) {
+    startTime = Date.now();
+    answer.push(addManyNums(i * increment));
+    endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+  }
+  return answer;
 }
 
 
 n = 1000000
 console.log(`addNums(${n}): `);
+console.time("addNums");
 addNums10Timing(1000000);
+console.timeEnd("addNums");
 
 console.log("\n***********\n");
 
 n = 1000
 console.log(`addManyNums(${n}): `);
+console.time("addManyNums");
 addManyNums10Timing(5000);
+console.timeEnd("addManyNums");
